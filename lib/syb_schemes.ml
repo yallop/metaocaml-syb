6b2d043fd11636bcc429137388bf4112c3da3fc4
Jeremy Yallop <yallop@gmail.com>
2015-08-26 01:30:41 +0100

    Stage the schemes.

--- lib/syb_schemes.ml
+++ lib/syb_schemes.ml
@@ -1,5 +1,6 @@
 open Syb_constructors
 open Syb_classes
+open Syb_fixpoints
 
 (* Various utilities *)
 let unjust l o = match o with Some x -> x :: l | None -> l
@@ -12,16 +13,27 @@ let maximum = List.fold_left max 0
 let rec everywhere : genericT -> genericT =
   fun (f : genericT) {X:DATA} x -> f ((gmapT (everywhere f) : genericT) x)
 
+let everywhere_ (f : genericT_) =
+  gfixT_ (fun self {X:DATA} x -> f (gmapT_ self x))
+
 
 (** Apply a transformation everywhere in top-down manner *)
 let rec everywhere' : genericT -> genericT =
   fun (f : genericT) {X:DATA} x -> (gmapT (everywhere' f) : genericT) (f x)
+
+let everywhere'_ (f : genericT_) =
+  gfixT_ (fun (self : genericT_) {X:DATA} x ->
+      let g : genericT_ = (fun {E:DATA} y -> self (f y)) in gmapT_ g x)
 
 
 (** Variation on everywhere with an extra stop condition *)
 let rec everywhereBut : bool genericQ -> genericT -> genericT =
   fun (stop : bool genericQ) (f : genericT) {X:DATA} x ->
     if stop x then x else f ((gmapT (everywhereBut stop f) : genericT) x)
+
+let everywhereBut_ (stop : bool genericQ_) (f : genericT_) =
+  gfixT_ (fun self {X:DATA} x ->
+      .< if .~(stop x) then .~x else .~(f (gmapT_ self x)) >.)
 
 
 (** Monadic variation on everywhere *)
@@ -37,6 +49,10 @@ let rec everything : 'r. ('r -> 'r -> 'r) -> 'r genericQ -> 'r genericQ =
   fun (@) g {X: DATA} x ->
     let f = g x in  List.fold_left (@) f (gmapQ (everything (@) g) x)
 
+let everything_ (@) (g : _ genericQ_) =
+  gfixQ_ (fun self {X: DATA} x ->
+      let f = g x in .< List.fold_left (@) .~f .~(gmapQ_ self x) >.)
+
 
 (** Variation of "everything" with an added stop condition *)
 let rec everythingBut : 'r. ('r -> 'r -> 'r) -> ('r * bool) genericQ -> 'r genericQ =
@@ -44,6 +60,12 @@ let rec everythingBut : 'r. ('r -> 'r -> 'r) -> ('r * bool) genericQ -> 'r gener
     match stop x with
     | v, true -> v
     | v, false -> List.fold_left (@) v (gmapQ (everythingBut (@) stop) x)
+
+let everythingBut_ (@) (stop : _ genericQ_) =
+  gfixQ_ (fun self {X: DATA} x ->
+      .< match .~(stop x) with
+      | v, true -> v
+      | v, false -> List.fold_left (@) v .~(gmapQ_ self x) >.)
 
 
 (** Summarise all nodes in top-down, left-to-right order, carrying some state
@@ -55,6 +77,9 @@ let rec everythingBut : 'r. ('r -> 'r -> 'r) -> ('r * bool) genericQ -> 'r gener
 (** Get a list of all entities that meet a predicate *)
 let listify {R:TYPEABLE} p =
   everything (@) (mkQ [] (fun x ->  if p x then [x] else []))
+
+let listify_ {R:TYPEABLE} p =
+  everything_ (@) (mkQ_ .<[]>. (fun x -> .< if .~(p x) then [.~x] else []>.))
 
 
 (** Look up a subterm by means of a maybe-typed filter *)
@@ -71,17 +96,30 @@ let rec synthesize :
   fun z o (f : _ genericQ) {X: DATA} x ->
    f x (List.fold_right o (gmapQ (synthesize z o f) x) z)
 
+let synthesize_ z o (f : _ genericQ_) =
+  gfixQ_ (fun self {X: DATA} x ->
+      .< .~(f x) (List.fold_right o .~(gmapQ_ self x) z) >.)
+
 
 (** Compute size of an arbitrary data structure *)
 let rec gsize {D:DATA} v = 1 + sum (gmapQ gsize v)
+
+let gsize_ = gfixQ_ (fun self {D:DATA} v ->
+    .< 1 + List.fold_left (+) 0 .~(gmapQ_ self v) >.)
 
 
 (** Count the number of immediate subterms of the given term *)
 let glength {T: DATA} x = List.length (gmapQ (fun {Z:DATA} _ ->  ()) x)
 
+let glength_ {T: DATA} x =
+  .< List.length .~(gmapQ_ (fun {Z:DATA} _ -> .< () >.) x) >.
+
 
 (** Determine depth of the given term *)
 let rec gdepth {D: DATA} x = succ (maximum (gmapQ gdepth x))
+
+let gdepth_ = gfixQ_ (fun self {D: DATA} x ->
+    .< succ (maximum .~(gmapQ_ self x)) >.)
 
 
 (** Determine the number of all suitable nodes in a given term *)
@@ -90,13 +128,22 @@ let gcount (p : bool genericQ) {T: DATA} x =
     (fun {X: DATA} x ->  if p x then 1 else 0 )
     x
 
+let gcount_ (p : bool genericQ_) {T: DATA} x =
+  everything_ (+)
+    (fun {X: DATA} x -> .< if .~(p x) then 1 else 0 >.)
+    x
+
 
 (** Determine the number of all nodes in a given term *)
 let gnodecount {X: DATA} x = gcount (fun {Y: DATA} _ ->  true ) x
 
+let gnodecount_ {X: DATA} x = gcount_ (fun {Y: DATA} _ -> .< true >.) x
+
 
 (** Determine the number of nodes of a given type in a given term *)
 let gtypecount {X:TYPEABLE} x = gcount (mkQ  false  (fun _ -> true))
+
+let gtypecount_ {X:TYPEABLE} x = gcount_ (mkQ_ .< false >. (fun _ -> .<true>.))
 
 
 (** Find (unambiguously) an immediate subterm of a given type *)
@@ -104,9 +151,19 @@ let gfindtype {X:TYPEABLE} {D: DATA} x =
    singleton (List.fold_left unjust []
                (gmapQ (mkQ None (fun c -> Some c)) x))
 
+let gfindtype_ {X:TYPEABLE} {D: DATA} x =
+  .< singleton (List.fold_left unjust []
+               .~(gmapQ_ (mkQ_ .<None>. (fun c -> .<Some .~c>.)) x)) >.
+
 
 (** Generic show *)
 let rec gshow {D:DATA} v =
   "("^ string_of_constructor (constructor v)
   ^ String.concat " " (gmapQ gshow v)
   ^ ")"
+
+let gshow_ = gfixQ_ (fun self {D:DATA} v ->
+  .< "("^ string_of_constructor .~(constructor_ v)
+     ^ String.concat " " .~(gmapQ_ self v)
+     ^ ")" >.)
+
--- lib/syb_schemes.mli
+++ lib/syb_schemes.mli
@@ -1,13 +1,16 @@
 open Syb_classes
 
 (** Apply a transformation everywhere in bottom-up manner *)
-val everywhere : genericT -> genericT
+val everywhere  : genericT  -> genericT
+val everywhere_ : genericT_ -> genericT_
 
 (** Apply a transformation everywhere in top-down manner *)
-val everywhere' : genericT -> genericT
+val everywhere'  : genericT  -> genericT
+val everywhere'_ : genericT_ -> genericT_
 
 (** Variation on everywhere with an extra stop condition *)
-val everywhereBut : bool genericQ -> genericT -> genericT
+val everywhereBut  : bool genericQ  -> genericT  -> genericT
+val everywhereBut_ : bool genericQ_ -> genericT_ -> genericT_
 
 (** Monadic variation on everywhere *)
 (* val everywhereM : (Data a, Monad m) => GenericM m -> a -> m a *)
@@ -16,10 +19,12 @@ val everywhereBut : bool genericQ -> genericT -> genericT
 (* val somewhere : (MonadPlus m, Data a) => GenericM m -> a -> m a *)
 
 (** Summarise all nodes in top-down, left-to-right order *)
-val everything : ('r -> 'r -> 'r) -> 'r genericQ -> 'r genericQ
+val everything  : ('r -> 'r -> 'r) -> 'r genericQ  -> 'r genericQ
+val everything_ : ('r -> 'r -> 'r) -> 'r genericQ_ -> 'r genericQ_
 
 (** Variation of "everything" with an added stop condition *)
-val everythingBut : ('r -> 'r -> 'r) -> ('r * bool) genericQ -> 'r genericQ
+val everythingBut  : ('r -> 'r -> 'r) -> ('r * bool) genericQ  -> 'r genericQ
+val everythingBut_ : ('r -> 'r -> 'r) -> ('r * bool) genericQ_ -> 'r genericQ_
 
 (** Summarise all nodes in top-down, left-to-right order, carrying some state
     down the tree during the computation, but not left-to-right to
@@ -31,7 +36,8 @@ val everythingWithContext : {X: Data} -> 's -> ('r -> 'r -> 'r) ->
 *)
 
 (** Get a list of all entities that meet a predicate *)
-val listify : {R:TYPEABLE} -> (R.t -> bool) -> R.t list genericQ
+val listify  : {R:TYPEABLE} -> (R.t      -> bool)      -> R.t list genericQ
+val listify_ : {R:TYPEABLE} -> (R.t code -> bool code) -> R.t list genericQ_
 
 (** Look up a subterm by means of a maybe-typed filter *)
 (*
@@ -43,28 +49,37 @@ val something : 'u option genericQ -> 'u option genericQ
     2nd argument o is for reduction of results from subterms;
     3rd argument f updates the synthesised data according to the given term
 *)
-val synthesize : 's -> ('t -> 's -> 's) -> ('s -> 't) genericQ -> 't genericQ
+val synthesize  : 's -> ('t -> 's -> 's) -> ('s -> 't) genericQ  -> 't genericQ
+val synthesize_ : 's -> ('t -> 's -> 's) -> ('s -> 't) genericQ_ -> 't genericQ_
 
 (** Compute size of an arbitrary data structure *)
-val gsize : int genericQ
+val gsize  : int genericQ
+val gsize_ : int genericQ_
 
 (** Count the number of immediate subterms of the given term *)
-val glength : int genericQ
+val glength  : int genericQ
+val glength_ : int genericQ_
 
 (** Determine depth of the given term *)
-val gdepth : int genericQ
+val gdepth  : int genericQ
+val gdepth_ : int genericQ_
 
 (** Determine the number of all suitable nodes in a given term *)
-val gcount : bool genericQ -> int genericQ
+val gcount  : bool genericQ  -> int genericQ
+val gcount_ : bool genericQ_ -> int genericQ_
 
 (** Determine the number of all nodes in a given term *)
-val gnodecount : int genericQ
+val gnodecount  : int genericQ
+val gnodecount_ : int genericQ_
 
 (** Determine the number of nodes of a given type in a given term *)
-val gtypecount : {X:TYPEABLE} -> X.t -> int genericQ
+val gtypecount  : {X:TYPEABLE} -> X.t -> int genericQ
+val gtypecount_ : {X:TYPEABLE} -> X.t -> int genericQ_
 
 (** Find (unambiguously) an immediate subterm of a given type *)
-val gfindtype : {X:TYPEABLE} -> X.t option genericQ
+val gfindtype  : {X:TYPEABLE} -> X.t option genericQ
+val gfindtype_ : {X:TYPEABLE} -> X.t option genericQ_
 
 (** Generic show *)
-val gshow : string genericQ
+val gshow  : string genericQ
+val gshow_ : string genericQ_
